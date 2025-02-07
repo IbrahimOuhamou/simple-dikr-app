84a0f3f4c6843f64678dbf3dda129daf40e5ef03
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-02-07 18:04:37 +0100

    made it ssg thanks to Allah

---
+++ src/routes/+layout.js
@@ -0,0 +1,5 @@
+// بسم الله الرحمن الرحيم
+// la ilaha illa Allah Mohammed Rassoul Allah
+// This can be false if you're using a fallback (i.e. SPA mode)
+export const prerender = true;
+export const trailingSlash = 'always';
