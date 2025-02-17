6d028317904847bc686acc5c84d3b98dbc251414
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-02-17 11:54:43 +0100

    made it prerender thanks to Allah

--- src/routes/adkar/+page.svelte
+++ src/routes/adkar/+page.svelte
@@ -1,8 +1,8 @@
 <script>
-	let { data } = $props();
+	import dikr_data from '$lib/data.json';
 </script>
 
-{#each data.dikr_data as dikr_list}
+{#each dikr_data as dikr_list}
 	<a class="block mx-auto my-2 rounded-md text-white p-2 bg-green-800" href="/adkar/{dikr_list.name}">
 		<p>{dikr_list.description}</p>
 		<h3 class="text-xl">{dikr_list.name}</h3>
