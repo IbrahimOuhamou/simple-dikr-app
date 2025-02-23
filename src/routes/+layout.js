c149660b9c7f8861ee046f4cb10e692e32b2f12a
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-02-23 09:38:24 +0000

    fixed broken build thanks to Allah

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,6 +1,5 @@
 // بسم الله الرحمن الرحيم
 // la ilaha illa Allah Mohammed Rassoul Allah
 // This can be false if you're using a fallback (i.e. SPA mode)
-export const ssr = false;
 export const prerender = true;
 // export const trailingSlash = 'always';
