55306c177a3a37ecb0b98b222ef1f5a987d10b91
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-02-23 09:36:10 +0000

    added navbar thanks to Allah

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,5 +1,6 @@
 // بسم الله الرحمن الرحيم
 // la ilaha illa Allah Mohammed Rassoul Allah
 // This can be false if you're using a fallback (i.e. SPA mode)
+export const ssr = false;
 export const prerender = true;
-export const trailingSlash = 'always';
+// export const trailingSlash = 'always';
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,13 @@
 	let { children } = $props();
 </script>
 
-<main class="block max-w-2xl md:mx-auto mx-2">
+<main class="block max-w-2xl md:w-full md:mx-auto mx-2">
 	{@render children()}
 </main>
+
+
+<nav class="bg-gray-400 flex justify-around w-screen">
+	<a class="p-2" href="/">Home</a>
+	<a class="p-2" href="/adkar">adkar</a>
+	<a class="p-2" href="/misbaha">misbaha</a>
+</nav>
