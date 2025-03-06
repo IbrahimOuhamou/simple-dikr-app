8efaedc7fc539ca649064824faf4a68910ae37a0
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-03-06 11:42:42 +0000

    added adkar and styled all thanks to Allah

--- src/lib/Dikr.svelte
+++ src/lib/Dikr.svelte
@@ -1,5 +1,5 @@
 <script>
-    let {value, count, i, removeFn} = $props();
+    let {value, count, virtue = null, i, removeFn} = $props();
     let count_current = $state(count);
 
     // function decrement() {
@@ -16,9 +16,13 @@
     })
 </script>
 
-<button class="block m-auto w-full rounded-md bg-green-400 text-white mt-5 pt-1" onclick={count_current -= 1}>
-    <p>{value}</p>
+<button class="block m-auto w-full rounded-md bg-green-500 text-white text-lg mt-5 pt-1" onclick={() => {count_current -= 1}}>
+    <p class="p-4">{value}</p>
     <hr />
+    {#if virtue}
+        <p dir="rtl" class="font-thin text-sm">{virtue}</p>
+        <hr />
+    {/if}
     <p dir="rtl" class="flex justify-around w-3/4 mx-auto">
         <span>{count_current}</span> 
         <span>|</span>
